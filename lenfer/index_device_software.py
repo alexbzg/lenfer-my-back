098b43ed862fc75c5bc4fd5ed8ac7eaaf7b884c7
root <alexbzg@gmail.com>
2020-12-30 13:56:34 +0300

    device software indexer builds hashes for the whole package

--- lenfer/index_device_software.py
+++ lenfer/index_device_software.py
@@ -6,6 +6,7 @@ import sys
 import hashlib
 
 from json_utils import save_json, load_json
+from hash import data_hash
 
 PARSER = argparse.ArgumentParser(description="lenfer device software index builder")
 PARSER.add_argument('path')
@@ -22,6 +23,7 @@ SOFTWARE_ROOT_PATH = os.path.join(ARGS.path, 'software')
 INDEX = load_json(INDEX_PATH)
 if not INDEX:
     INDEX = {}
+DEVICES_TYPES = {}
 
 for root, dirs, files in os.walk(SOFTWARE_ROOT_PATH):
     for file_name in files:
@@ -34,6 +36,17 @@ for root, dirs, files in os.walk(SOFTWARE_ROOT_PATH):
         if not file_web_path in INDEX:
             INDEX[file_web_path] = {}
         INDEX[file_web_path]['hash'] = file_hash
+        if 'devices_types' in INDEX[file_web_path]:
+            for type in INDEX[file_web_path]['devices_types']:
+                if type not in DEVICES_TYPES:
+                    DEVICES_TYPES[type] = None
+
+for device_type in DEVICES_TYPES:
+    DEVICES_TYPES[device_type] = data_hash({file: data for file, data in INDEX.items()\
+            if 'devices_types' not in data or device_type in data['devices_types']})
+
+DEVICES_TYPES['base'] = data_hash({file: data for file, data in INDEX.items()\
+    if 'devices_types' not in data})
 
 save_json(INDEX, INDEX_PATH, indent=4)
-
+save_json(DEVICES_TYPES, os.path.join(ARGS.path, 'devices.json'))
